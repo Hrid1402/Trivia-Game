7a03fad3118b223625dc77b413bb26df2a3e205f
Hrid <Hrid1402@gmail.com>
2025-04-15 14:39:16 -0500

    FixAI.js

--- src/AI.js
+++ src/AI.js
@@ -3,7 +3,7 @@ import { GoogleGenerativeAI } from "@google/generative-ai"
 
 const genAI = new GoogleGenerativeAI(import.meta.env.VITE_API_KEY);  
 
-const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
+const model = genAI.getGenerativeModel({ model: "models/gemini-2.0-flash" });
 
 
 
